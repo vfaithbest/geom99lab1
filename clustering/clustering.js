81419f853ecdac4d2d39e3b3048c7240bd32fdfb
vfaithbest <146376070+vfaithbest@users.noreply.github.com>
2024-01-22 18:59:55 -0500

    Update clustering.js

--- clustering/clustering.js
+++ clustering/clustering.js
@@ -1,7 +1,7 @@
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 3,
-    center: { lat: -28.024, lng: 140.887 },
+    zoom: 4,
+    center: { lat: 46.217628, lng: -82.070231},
   });
   // Create an array of alphabetical characters used to label the markers.
   const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
